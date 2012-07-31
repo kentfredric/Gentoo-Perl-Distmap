61826a5ebe29ba3d9f253b795453f289ba8a097d
Kent Fredric <kentfredric@gmail.com>
2012-08-01 08:42:12 +1200

    Build results of d87c903 (on master)

---
+++ blib/lib/Gentoo/Perl/Distmap.pm
@@ -0,0 +1,172 @@
+use strict;
+use warnings;
+
+package Gentoo::Perl::Distmap;
+BEGIN {
+  $Gentoo::Perl::Distmap::AUTHORITY = 'cpan:KENTNL';
+}
+{
+  $Gentoo::Perl::Distmap::VERSION = '0.1.0';
+}
+
+# ABSTRACT: A reader/writer for the metadata/perl/distmap.json file.
+
+use 5.010000;
+use Gentoo::Perl::Distmap::Record;
+use Gentoo::Perl::Distmap::Map;
+
+use Moo;
+use MooseX::Has::Sugar qw( rw );
+use Sub::Quote qw( quote_sub );
+
+
+
+has map => ( rw,
+  default => quote_sub(q| require Gentoo::Perl::Distmap::Map; Gentoo::Perl::Distmap::Map->new() |),
+  handles => [qw( multi_repo_dists all_mapped_dists mapped_dists dists_in_repo )],
+);
+
+
+sub load {
+  my ( $self, $method, $source ) = @_;
+  return $self->new(
+    map => Gentoo::Perl::Distmap::Map->from_rec(
+      $self->decoder->decode( $self->can( '_load_' . $method )->( $self, $method, $source ) )
+    )
+  );
+}
+
+
+sub save {
+  my ( $self, $method, $target ) = @_;
+  return $self->can( '_save_' . $method )->( $self, $self->encoder->encode( $self->map->to_rec ), $target );
+}
+
+
+sub _save_string     { return $_[1] }
+sub _save_filehandle { return $_[2]->print( $_[1] ) }
+sub _save_file { require Path::Class::File; return $_[0]->_save_filehandle( $_[1], Path::Class::File->new( $_[2] )->openw() ) }
+
+
+sub _load_file { require Path::Class::File; return scalar Path::Class::File->new( $_[2] )->slurp() }
+sub _load_filehandle { local $/ = undef; return scalar $_[2]->getline }
+sub _load_string { return $_[2] }
+
+
+sub decoder {
+  return state $json = do { require JSON; JSON->new->pretty->utf8->canonical; }
+}
+
+sub encoder {
+  return state $json = do { require JSON; JSON->new->pretty->utf8->canonical; }
+}
+
+no Moo;
+no MooseX::Has::Sugar;
+
+1;
+
+
+__END__
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+Gentoo::Perl::Distmap - A reader/writer for the metadata/perl/distmap.json file.
+
+=head1 VERSION
+
+version 0.1.0
+
+=head1 SYNOPSIS
+
+	my $dm  = Gentoo::Perl::Distmap->load(  file => '../path/to/distmap.json' );
+	$dm->save( file => '/tmp/foo.x' );
+
+	for my $dist ( sort $dm->dists_in_repo('gentoo') ) {
+		/* see the upstream distnames visible in gentoo */
+	}
+	for my $dist ( sort $dm->dists_in_repo('perl-experimental') ) {
+		/* see the upstream distnames visible in perl-experimental */
+	}
+	for my $dist ( sort $dm->multi_repo_dists ) {
+		/* see the dists that exist in more than one repo */
+	}
+-	for my $dist ( sort $dm->mapped_dists ) {
+		/* see the dists that have at least one version in the dataset */
+		/* note: dists with empty version sets should be deemed a bug  */
+	}
+
+Interface for creating/augmenting/comparing .json files still to be defined, basic functionality only at this time.
+
+=head1 ATTRIBUTES
+
+=head2 map
+
+=head1 METHODS
+
+=head2 save
+
+	$instance->save( file => $filepath );
+	$instance->save( filehandle => $fh );
+	my $string = $instance->save( string => );
+
+=head1 CLASS METHODS
+
+=head2 load
+
+	my $instance = G:P:Distmap->load( file => $filepath );
+	my $instance = G:P:Distmap->load( filehandle => $fh );
+	my $instance = G:P:Distmap->load( string => $str );
+
+=head2 decoder
+
+	$decoder = G:P:Distmap->decoder();
+
+=head2 encoder
+
+	$encoder = G:P:Distmap->encoder();
+
+=head1 ATTRIBUTE METHODS
+
+=head2 map -> map
+
+=head2 multi_repo_dists -> map
+
+=head2 all_mapped_dists -> map
+
+=head2 mapped_dists -> map
+
+=head2 dists_in_repo -> map
+
+=head1 PRIVATE METHODS
+
+=head2 _save_string
+
+=head2 _save_filehandle
+
+=head2 _save_file
+
+=head1 PRIVATE CLASS METHODS
+
+=head2 _load_file
+
+=head2 _load_filehandle
+
+=head2 _load_string
+
+=head1 AUTHOR
+
+Kent Fredric <kentfredric@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Kent Fredric <kentfredric@gmail.com>.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
---
+++ blib/lib/Gentoo/Perl/Distmap/Map.pm
@@ -0,0 +1,134 @@
+use strict;
+use warnings;
+
+package Gentoo::Perl::Distmap::Map;
+BEGIN {
+  $Gentoo::Perl::Distmap::Map::AUTHORITY = 'cpan:KENTNL';
+}
+{
+  $Gentoo::Perl::Distmap::Map::VERSION = '0.1.0';
+}
+
+# ABSTRACT: A collection of CPAN dists mapped to Gentoo ones.
+
+use Moo;
+use MooseX::Has::Sugar qw( rw );
+use Sub::Quote qw( quote_sub );
+
+
+has store => rw, default => quote_sub(q{ {} });
+
+
+sub all_mapped_dists { return keys %{ $_[0]->store } }
+
+
+sub mapped_dists {
+  my ($self) = @_;
+  return grep { $self->store->{$_}->has_versions } $self->all_mapped_dists;
+}
+
+
+sub multi_repo_dists {
+  my ($self) = @_;
+  return grep { $self->store->{$_}->is_multi_repo } $self->all_mapped_dists;
+}
+
+
+sub dists_in_repo {
+  my ( $self, $repo ) = @_;
+  return grep { $self->store->{$_}->in_repo($repo) } $self->all_mapped_dists;
+}
+
+
+sub to_rec {
+  my ($self) = @_;
+  my $out;
+  for my $dist ( keys %{ $self->store } ) {
+    $out->{$dist} = $self->store->{$dist}->to_rec;
+  }
+  return $out;
+}
+
+
+sub from_rec {
+  my ( $class, $rec ) = @_;
+  if ( ref $rec ne 'HASH' ) {
+    require Carp;
+    Carp::confess('Can only convert from hash records');
+  }
+  my $rec_clone = { %{$rec} };
+  my $in;
+  require Gentoo::Perl::Distmap::RecordSet;
+  for my $dist ( keys %{$rec_clone} ) {
+    $in->{$dist} = Gentoo::Perl::Distmap::RecordSet->from_rec( $rec_clone->{$dist} );
+  }
+  return $class->new( store => $in, );
+}
+
+no Moo;
+no MooseX::Has::Sugar;
+
+1;
+
+__END__
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+Gentoo::Perl::Distmap::Map - A collection of CPAN dists mapped to Gentoo ones.
+
+=head1 VERSION
+
+version 0.1.0
+
+=head1 ATTRIBUTES
+
+=head2 store
+
+=head1 METHODS
+
+=head2 all_mapped_dists
+
+	my @names = $instance->all_mapped_dists();
+
+=head2 mapped_dists
+
+	my @names = $instance->mapped_dists();
+
+=head2 multi_repo_dists
+
+	my @names = $instance->multi_repo_dists();
+
+=head2 dists_in_repo
+
+	my @names = $instance->dists_in_repo('gentoo');
+
+=head2 to_rec
+
+	my $datastructure = $instance->to_rec
+
+=head1 CLASS METHODS
+
+=head2 from_rec
+
+	my $instance = G:P:D:Map->from_rec( $datastructure );
+
+=head1 ATTRIBUTE METHODS
+
+=head2 store -> store
+
+=head1 AUTHOR
+
+Kent Fredric <kentfredric@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Kent Fredric <kentfredric@gmail.com>.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
